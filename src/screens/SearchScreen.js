92f1e13e36c564ac5308d73136a48d362bafc551
Piemonte Spectre <iykeprince48@gmail.com>
2021-07-03 16:55:03 +0100

    init commit for the app

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import SearchBar from '../components/SearchBar'
+
+const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+
+    return <View>
+        <SearchBar 
+            term={term} 
+            onTermChange={newTerm => setTerm(newTerm)} 
+            onTermSubmit={() => console.log('term was submitted') } 
+        />
+        <Text>{term}</Text>
+    </View>
+}
+
+export default SearchScreen
